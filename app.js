508091d3ceac2c1c79e91c9faea75d2dbcc79438
nersus15 <fathur.ashter15@gmail>
2019-09-19 23:41:20 +0800

    menambahkan CORS handler

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ const suratRoutes = require( './api/routes/surats' );
 app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( bodyParser.json() );
 app.use( morgan( 'dev' ) );
+app.use( ( req, res, next ) => {
+    res.header( "Access-Control-Allow-Origin", "*" );
+    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization" );
+    if ( req.method === 'OPTIONS' ) {
+        res.header( 'Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE' );
+        return res.status( 200 ).json( {} );
+    }
+    next();
+} )
 
 // Routes wich should Handle Request
 app.use( '/surats', suratRoutes );
