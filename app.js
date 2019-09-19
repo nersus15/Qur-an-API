d529b94804143a4e426f128c222f127755bac0b9
nersus15 <fathur.ashter15@gmail>
2019-09-19 23:08:05 +0800

    menambahkan error handler

--- app.js
+++ app.js
@@ -6,10 +6,24 @@ const app = express();
 // import routers
 const suratRoutes = require( './api/routes/surats' );
 
-// use middleware
+/* use middleware */
 app.use( morgan( 'dev' ) );
 
 // Routes wich should Handle Request
 app.use( '/surats', suratRoutes );
 
+// Htpp Request Error Handler 
+app.use( ( req, res, next ) => {
+    const err = new Error( "Not Found" );
+    err.status = 404;
+    next( err );
+} );
+app.use( ( err, req, res, next ) => {
+    res.status( err.status || 500 );
+    res.json( {
+        message: err.message
+    } );
+} )
+
+/* end user middleware */
 module.exports = app;
